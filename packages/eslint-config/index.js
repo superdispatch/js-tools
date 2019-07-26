5c6294eb2fe3c039195ce8d8fa8714da309a7797
Umidbek Karimov <uma.karimov@gmail.com>
2019-07-26 10:09:38 +0500

    feat(eslint-config-base): Add shared base config for eslint.

--- packages/eslint-config/index.js
+++ packages/eslint-config/index.js
@@ -4,10 +4,11 @@ module.exports = {
   overrides: [
     // JS files will be treated as Node files.
     { files: ['*.js'], extends: './node' },
-
+    // TS files can be both server and client side, so keep it as separate.
     { files: ['*.ts'], extends: './typescript' },
-    { files: ['*.ts', '*.tsx', '*.jsx'], extends: './react' },
-
+    // Turn on React checks only for some file extensions.
+    { files: ['*.tsx', '*.jsx'], extends: './react' },
+    // Run tests only against `spec` files.
     { files: ['*.spec.js', '*.spec.ts', '*.spec.tsx'], extends: './jest' },
   ],
 };
