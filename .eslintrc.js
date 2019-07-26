6d039c129b4e9c9af27bff5b0509d4642cc7f7ed
Umidbek Karimov <uma.karimov@gmail.com>
2019-07-26 19:37:07 +0500

    feat(eslint-config-node): Add shared base config for eslint.

--- .eslintrc.js
+++ .eslintrc.js
@@ -5,15 +5,7 @@ module.exports = {
 
   overrides: [
     {
-      files: '**/*.spec.js',
-      rules: {
-        'node/no-extraneous-require': 'off',
-        'import/no-extraneous-dependencies': 'off',
-      },
-    },
-
-    {
-      files: '**/__testutils__/**/*.js',
+      files: ['**/*.spec.js', '**/__testutils__/**/*.js'],
       extends: './packages/eslint-config/jest',
       rules: {
         'node/no-extraneous-require': 'off',
