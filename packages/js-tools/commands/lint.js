1425c143718d4635adb959000643c372c231e981
Umidbek Karimov <uma.karimov@gmail.com>
2019-07-29 18:47:43 +0500

    [WIP]

--- packages/js-tools/commands/lint.js
+++ packages/js-tools/commands/lint.js
@@ -1,32 +1,64 @@
 'use strict';
 
+const path = require('path');
 const execa = require('execa');
 
-const eslintExtensions = ['js', 'jsx', 'ts', 'tsx'];
+const eslintExtensions = ['.js', '.jsx', '.ts', '.tsx'];
 const prettierExtensions = [
-  'js',
-  'jsx',
-  'json',
-  'ts',
-  'tsx',
-  'css',
-  'less',
-  'scss',
-  'graphql',
-  'md',
-  'markdown',
+  '.js',
+  '.jsx',
+  '.json',
+  '.ts',
+  '.tsx',
+  '.css',
+  '.less',
+  '.scss',
+  '.graphql',
+  '.md',
+  '.markdown',
 ];
 
-function execLinter(cmd, args, extensions) {
-  const glob = `**/*.{${extensions.join(',')}}`;
-  const cleanArgs = [...args.filter(Boolean), glob];
+function execLinter(cmd, args) {
+  console.log(`${cmd} ${args.join(' ')}`);
 
-  console.log(`${cmd} ${cleanArgs.join(' ')}`);
-
-  return execa(cmd, cleanArgs, { stdio: 'inherit', preferLocal: true });
+  return execa(cmd, args, { stdio: 'inherit', preferLocal: true });
 }
 
-module.exports = async ({ fix }) => {
-  await execLinter('eslint', [fix && '--fix'], eslintExtensions);
-  await execLinter('prettier', [fix ? '--write' : '--list-different'], prettierExtensions);
+module.exports = async ({ fix, files }) => {
+  const eslintArgs = [];
+  const eslintFiles = [];
+  const prettierArgs = [];
+  const prettierFiles = [];
+
+  if (fix) {
+    eslintArgs.push('--fix');
+    prettierArgs.push('--write');
+  } else {
+    prettierArgs.push('--list-different');
+  }
+
+  if (files.length === 0) {
+    eslintFiles.push(`**/*{${eslintExtensions.join(',')}`);
+    prettierFiles.push(`**/*{${prettierExtensions.join(',')}`);
+  } else {
+    files.forEach(file => {
+      const ext = path.extname(file);
+
+      if (eslintExtensions.includes(ext)) {
+        eslintFiles.push(file);
+      }
+
+      if (prettierExtensions.includes(ext)) {
+        prettierFiles.push(file);
+      }
+    });
+  }
+
+  if (eslintFiles.length > 0) {
+    await execLinter('eslint', [...eslintArgs, ...eslintFiles]);
+  }
+
+  if (prettierFiles.length > 0) {
+    await execLinter('prettier', [...prettierArgs, ...prettierFiles]);
+  }
 };
