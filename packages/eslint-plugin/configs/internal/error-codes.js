d89556167ed43eef968d4cb9b39aef4669725c48
Umidbek Karimov <uma.karimov@gmail.com>
2019-10-09 14:08:57 +0500

    refactor(eslint-plugin): Check env variables once.

--- packages/eslint-plugin/configs/internal/error-codes.js
+++ packages/eslint-plugin/configs/internal/error-codes.js
@@ -1,10 +1,11 @@
 'use strict';
 
+const isDevMode = process.env.CI !== 'true' && process.env.NODE_ENV === 'development';
+
 const OFF = 'off';
 const ERROR = 'error';
 const WARNING = 'warn';
-const DEPT = process.env.CI !== 'true' && process.env.NODE_ENV === 'development' ? OFF : WARNING;
-const INCONSISTENCY =
-  process.env.CI !== 'true' && process.env.NODE_ENV === 'development' ? WARNING : ERROR;
+const DEPT = isDevMode ? OFF : WARNING;
+const INCONSISTENCY = isDevMode ? WARNING : ERROR;
 
 module.exports = { OFF, DEPT, ERROR, INCONSISTENCY };
