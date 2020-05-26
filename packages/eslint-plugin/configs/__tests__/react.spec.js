593eaec12160ed359ff6dcb670c5c948ff58664a
Umidbek Karimov <uma.karimov@gmail.com>
2020-05-26 17:52:10 +0500

    test(eslint-plugin): Use `CLIEngine` to test configs.

--- packages/eslint-plugin/configs/__tests__/react.spec.js
+++ packages/eslint-plugin/configs/__tests__/react.spec.js
@@ -1,9 +1,10 @@
 'use strict';
 
 const { getConfigValues } = require('../__testutils__/test-eslint-config');
+const config = require('../react');
 
 it('extends dependencies', async () => {
-  const [meta, rules] = await getConfigValues('react');
+  const [meta, rules] = await getConfigValues(config);
 
   expect(meta).toMatchInlineSnapshot(`
     Object {
