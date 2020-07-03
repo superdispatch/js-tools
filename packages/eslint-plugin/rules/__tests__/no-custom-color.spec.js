7baaef1b6b2448e6c4101296f05c99e467b257ab
doniyor2109 <doniyor2109@gmail.com>
2020-07-03 19:12:25 +0500

    WIP

--- packages/eslint-plugin/rules/__tests__/no-custom-color.spec.js
+++ packages/eslint-plugin/rules/__tests__/no-custom-color.spec.js
@@ -3,6 +3,21 @@
 const { RuleTester } = require('eslint');
 const rule = require('../no-custom-color');
 
+const options = [
+  {
+    colors: {
+      '#6A707C': {
+        source: '@superdispatch/ui',
+        specifier: 'Color.Grey200',
+      },
+      '#5B6371': {
+        source: '@superdispatch/ui',
+        specifier: 'Color.Grey300',
+      },
+    },
+  },
+];
+
 const parserOptions = {
   ecmaVersion: 2020,
   sourceType: 'module',
@@ -13,96 +28,79 @@ const ruleTester = new RuleTester({ parserOptions });
 
 ruleTester.run('no-custom-color', rule, {
   valid: [
-    {
-      code: 'const color =  "#e4e7ea";',
-    },
-    {
-      code: 'export const icon = <svg fill="#e4e7ea"></svg>;',
-    },
+    { options, code: 'const color =  "#e4e7ea";' },
+    { options, code: 'export const icon = <svg fill="#e4e7ea"></svg>;' },
 
     {
+      options,
       code:
         'export const Placeholder = <p style={{backgroundColor: "#e4e7ea"}}>Hi</p>;',
     },
     {
+      options,
       code: 'export const Placeholder = styled.p`background-color: #e4e7ea;`;',
     },
     {
+      options,
       code:
         'export const Placeholder = <p css="background-color: #e4e7ea;">Hi</p>;',
     },
   ],
   invalid: [
     {
+      options,
       code: 'const color =  "#6A707C";',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'const color =  Color.Grey200;',
       errors: ['Use Color.Grey200 from "@superdispatch/ui"'],
     },
     {
+      options,
       code: 'const color =  `#6A707C`;',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'const color =  Color.Grey200;',
       errors: ['Use Color.Grey200 from "@superdispatch/ui"'],
     },
     {
-      code: 'const color =  "#6A707C";const color2 =  "#6A707C";',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'const color =  Color.Grey200;' +
-        'const color2 =  Color.Grey200;',
+      options,
+      code: 'const color =  "#6A707C";const color2 =  "#5B6371";',
       errors: [
         'Use Color.Grey200 from "@superdispatch/ui"',
-        'Use Color.Grey200 from "@superdispatch/ui"',
+        'Use Color.Grey300 from "@superdispatch/ui"',
       ],
     },
     {
+      options,
       code: 'export const icon = <svg fill="#6A707C"></svg>;',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'export const icon = <svg fill={Color.Grey200}></svg>;',
       errors: ['Use Color.Grey200 from "@superdispatch/ui"'],
     },
     {
+      options,
       code: 'export const icon = <svg fill={"#6A707C"}></svg>;',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'export const icon = <svg fill={Color.Grey200}></svg>;',
       errors: ['Use Color.Grey200 from "@superdispatch/ui"'],
     },
     {
+      options,
       code:
         'export const Placeholder = <p style={{backgroundColor: "#6A707C"}}>Hi</p>;',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'export const Placeholder = <p style={{backgroundColor: Color.Grey200}}>Hi</p>;',
       errors: ['Use Color.Grey200 from "@superdispatch/ui"'],
     },
 
     {
+      options,
       code: 'export const Placeholder = styled.p`background-color: #6A707C;`;',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'export const Placeholder = styled.p`background-color: ${Color.Grey200};`;',
       errors: ['Use Color.Grey200 from "@superdispatch/ui"'],
     },
     {
+      options,
       code:
-        'export const Placeholder = <p css="background-color: #6A707C;color: #6A707C;">Hi</p>;',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'export const Placeholder = <p css={`background-color: ${Color.Grey200};color: ${Color.Grey200};`}>Hi</p>;',
-      errors: ['Use Color.Grey200 from "@superdispatch/ui"'],
+        'export const Placeholder = <p css="background-color: #6A707C;color: #5B6371;">Hi</p>;',
+      errors: [
+        'Use Color.Grey200 from "@superdispatch/ui"',
+        'Use Color.Grey300 from "@superdispatch/ui"',
+      ],
     },
     {
+      options,
       code:
         "import { Color } from '@superdispatch/ui';" +
         'export const Placeholder = <p css={`background-color: #6A707C;color: ${Color.Grey200};`}>Hi</p>;',
-      output:
-        "import { Color } from '@superdispatch/ui';" +
-        'export const Placeholder = <p css={`background-color: ${Color.Grey200};color: ${Color.Grey200};`}>Hi</p>;',
       errors: ['Use Color.Grey200 from "@superdispatch/ui"'],
     },
   ],
--- packages/eslint-plugin/rules/no-custom-color.js
+++ packages/eslint-plugin/rules/no-custom-color.js
@@ -1,154 +1,104 @@
-/* eslint eslint-comments/no-use: off */
 /**
  * @typedef {import("estree").Node} ESTreeNode
  * @typedef {import("eslint").Rule.RuleModule} RuleModule
+ * @typedef {import("eslint").Rule.RuleContext} RuleContext
  * @typedef {import("@typescript-eslint/typescript-estree").TSESTree.Node} Node
  * @typedef {import("@typescript-eslint/typescript-estree").TSESTree.Literal} Literal
  * @typedef {import("@typescript-eslint/typescript-estree").TSESTree.TemplateLiteral} TemplateLiteral
+ *
+ *  @typedef {{source: string, specifier: string}} ColorOption
+ *  @typedef {{colors: Record<string, ColorOption> }} Option
  */
 
 'use strict';
 
-// eslint-disable-next-line import/no-extraneous-dependencies
-const { Color } = require('@superdispatch/ui');
-
-/**
- *
- * @type {Map<string, string>}
- */
-const colorMap = new Map(
-  Object.entries(Color).map(([key, value]) => [value, key]),
-);
-const colorRegex = new RegExp(
-  Array.from(colorMap.keys(), (x) =>
-    x.replace(/\(/, '\\(').replace(/\)/, '\\)'),
-  ).join('|'),
-  'ig',
-);
-
-/**
- *
- * @param color {string}
- * @returns {string}
- */
-function formatColor(color) {
-  return `Color.${color}`;
-}
 /**
  * @type {RuleModule}
  * */
 module.exports = {
   meta: {
-    scheme: [],
     fixable: 'code',
     type: 'suggestion',
-    messages: {
-      noCustomColor: 'Use {{color}} from "@superdispatch/ui"',
-    },
+    scheme: [
+      {
+        type: 'object',
+        properties: {
+          colors: {
+            type: 'object',
+            additionalProperties: {
+              type: 'object',
+              properties: {
+                type: 'object',
+                properties: {
+                  source: { type: 'string' },
+                  specifier: { type: 'string' },
+                },
+              },
+            },
+          },
+        },
+      },
+    ],
   },
 
   create(context) {
-    let isImported = false;
+    const [option] = /** @type {Option[]} */ context.options;
+
+    const COLORS_MAP = new Map(
+      Object.entries(option.colors).map(([color, value]) => [
+        color.toLowerCase(),
+        value,
+      ]),
+    );
+
+    const COLOR_REGEX = new RegExp(
+      Array.from(COLORS_MAP, ([color]) =>
+        color.replace(/\(/, '\\(').replace(/\)/, '\\)'),
+      ).join('|'),
+      'ig',
+    );
 
     /**
      *
-     * @param value {string}
-     * @returns {[string, string]|[null, null]}
+     * @param text {string}
+     * @returns {ColorOption[]}
      */
-    function parseColor(value) {
-      const color = colorMap.get(value);
-      if (color) {
-        return [formatColor(color), formatColor(color)];
+    function findUsages(text) {
+      let match;
+      const colors = new Set();
+
+      while ((match = COLOR_REGEX.exec(text))) {
+        const [color] = match;
+
+        colors.add(color.toLowerCase());
       }
-      if (colorRegex.test(value)) {
-        /** @type {Set<string>} */
-        const colors = new Set();
-        const template = value.replace(colorRegex, (match) => {
-          const matchColor = formatColor(colorMap.get(match) || '');
-          colors.add(matchColor);
-          return `\${${matchColor}}`;
-        });
-        return [Array.from(colors).join(','), `\`${template}\``];
-      }
-      return [null, null];
+
+      return Array.from(colors, (color) => COLORS_MAP.get(color));
     }
 
     /**
      *
-     * @param node {Node}
-     * @param value {string}
-     * @param [fn] {(fixer: any, value: string) => any}
+     * @param node {any}
+     * @param text {string}
      */
-    function process(node, value, fn) {
-      const [color, replacement] = parseColor(value);
-      const estreeNode = /** @type {ESTreeNode} */ (
-        /** @type {unknown} */ node
-      );
+    function process(node, text) {
+      const usages = findUsages(text);
 
-      if (color && replacement) {
+      usages.forEach(({ specifier, source }) => {
         context.report({
-          node: estreeNode,
-          messageId: 'noCustomColor',
-          data: { color },
-          fix: (fixer) => {
-            const fixes = [
-              fn
-                ? fn(fixer, replacement)
-                : fixer.replaceText(estreeNode, replacement),
-            ];
-
-            if (!isImported) {
-              const [program] = context.getAncestors();
-              const scope = context.getScope();
-
-              if (!scope.variables.some((v) => v.name === 'Color')) {
-                fixes.push(
-                  fixer.insertTextBefore(
-                    program,
-                    "import { Color } from '@superdispatch/ui';",
-                  ),
-                );
-              }
-              isImported = true;
-            }
-
-            return fixes;
-          },
+          node,
+          message: `Use ${specifier} from "${source}"`,
         });
-      }
+      });
     }
 
     return {
       /**
        * @param {Literal} node
        * */
-      'VariableDeclarator > Literal': function (node) {
-        if (typeof node.value === 'string') {
-          process(node, node.value);
-        }
-      },
-      /**
-       * @param {Literal} node
-       * */
-      'Property > Literal': function (node) {
-        if (typeof node.value === 'string') {
-          process(node, node.value);
-        }
-      },
-      /**
-       * @param {Literal} node
-       * */
-      'JSXAttribute > Literal': function (node) {
-        if (typeof node.value === 'string') {
-          process(node, node.value, (fixer, replacement) =>
-            fixer.replaceText(node, `{${replacement}}`),
-          );
-        }
-      },
-      /**
-       * @param {Literal} node
-       * */
-      'JSXExpressionContainer > Literal': function (node) {
+      'VariableDeclarator > Literal, Property > Literal, JSXAttribute > Literal, JSXExpressionContainer > Literal': function (
+        node,
+      ) {
         if (typeof node.value === 'string') {
           process(node, node.value);
         }
@@ -157,9 +107,7 @@ module.exports = {
        * @param {TemplateLiteral} node
        * */
       TemplateLiteral(node) {
-        const [start, end] = node.range;
-        const text = context.getSourceCode().text.slice(start + 1, end - 1);
-        process(node, text);
+        process(node, context.getSourceCode().text);
       },
     };
   },
