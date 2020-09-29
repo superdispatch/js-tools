2f65e45274a3f7cf2e5db48c8c47920e82df340b
Aliyev Doniyor <doniyor2109@gmail.com>
2020-09-29 13:48:48 +0500

    feat(gh-scripts): Introduce scripts for GitHub action. (#36)

---
+++ packages/gh-scripts/README.md
@@ -0,0 +1,73 @@
+# gh-scripts
+
+Node scripts for GitHub actions
+
+[![oclif](https://img.shields.io/badge/cli-oclif-brightgreen.svg)](https://oclif.io)
+[![Version](https://img.shields.io/npm/v/@superdispatch/gh-scripts.svg)](https://npmjs.org/package/@superdispatch/gh-scripts)
+[![Downloads/week](https://img.shields.io/npm/dw/@superdispatch/gh-scripts.svg)](https://npmjs.org/package/@superdispatch/gh-scripts)
+[![License](https://img.shields.io/npm/l/@superdispatch/gh-scripts.svg)](https://github.com/superdispatch/superdispatch/js-tools/blob/master/packages/gh-scripts/package.json)
+
+<!-- toc -->
+
+- [gh-scripts](#gh-scripts)
+- [Usage](#usage)
+- [Commands](#commands)
+<!-- tocstop -->
+
+# Usage
+
+<!-- usage -->
+
+```sh-session
+$ npm install -g @superdispatch/gh-scripts
+$ gh-scripts COMMAND
+running command...
+$ gh-scripts (-v|--version|version)
+@superdispatch/gh-scripts/0.0.0 darwin-x64 node-v12.16.3
+$ gh-scripts --help [COMMAND]
+USAGE
+  $ gh-scripts COMMAND
+...
+```
+
+<!-- usagestop -->
+
+# Commands
+
+<!-- commands -->
+
+- [`gh-scripts deploy-preview`](#gh-scripts-deploy-preview)
+- [`gh-scripts help [COMMAND]`](#gh-scripts-help-command)
+
+## `gh-scripts deploy-preview`
+
+deploy preview
+
+```
+USAGE
+  $ gh-scripts deploy-preview
+
+OPTIONS
+  -a, --alias=alias  (required) deploy alias
+  -d, --dir=dir      (required) build dir
+  -h, --help         show CLI help
+```
+
+## `gh-scripts help [COMMAND]`
+
+display help for gh-scripts
+
+```
+USAGE
+  $ gh-scripts help [COMMAND]
+
+ARGUMENTS
+  COMMAND  command to show help for
+
+OPTIONS
+  --all  see all commands in CLI
+```
+
+_See code: [@oclif/plugin-help](https://github.com/oclif/plugin-help/blob/v3.2.0/src/commands/help.ts)_
+
+<!-- commandsstop -->
---
+++ packages/gh-scripts/src/commands/deploy-preview.ts
@@ -0,0 +1,117 @@
+import { Command, flags } from '@oclif/command';
+import { request } from '@octokit/request';
+import execa from 'execa';
+
+function getPreviewURL(text: string): string {
+  const match = /Website Draft URL: (.+)/.exec(text);
+
+  if (!match) {
+    throw new Error('Cannot find preview url from logs');
+  }
+
+  return match[1];
+}
+
+export default class DeployPreview extends Command {
+  static description = 'deploy preview';
+
+  static flags = {
+    help: flags.help({ char: 'h' }),
+    dir: flags.string({ char: 'd', description: 'build dir', required: true }),
+    alias: flags.string({
+      char: 'a',
+      description: 'deploy alias',
+      required: true,
+    }),
+  };
+
+  static args = [];
+
+  async run() {
+    const {
+      flags: { dir, alias },
+    } = this.parse(DeployPreview);
+
+    const {
+      GITHUB_SHA,
+      GITHUB_TOKEN,
+      GITHUB_REPOSITORY,
+      GITHUB_PULL_REQUEST_NUMBER,
+    } = process.env;
+
+    if (!GITHUB_SHA) {
+      throw new Error('Please provide `GITHUB_SHA`.');
+    }
+
+    if (!GITHUB_TOKEN) {
+      throw new Error('Please provide `GITHUB_TOKEN`.');
+    }
+
+    if (!GITHUB_REPOSITORY) {
+      throw new Error('Please provide `GITHUB_REPOSITORY`.');
+    }
+
+    if (!GITHUB_PULL_REQUEST_NUMBER) {
+      throw new Error('Please provide `GITHUB_PULL_REQUEST_NUMBER`.');
+    }
+
+    const [owner, repo] = GITHUB_REPOSITORY.split('/');
+    const issueNumber = parseInt(GITHUB_PULL_REQUEST_NUMBER);
+
+    const { data: comments } = await request(
+      'GET /repos/:owner/:repo/issues/:issue_number/comments',
+      {
+        repo,
+        owner,
+        issue_number: issueNumber,
+        headers: { authorization: `Token ${GITHUB_TOKEN}` },
+      },
+    );
+
+    const deployProcess = execa('yarn', [
+      '--silent',
+      'netlify',
+      'deploy',
+      `--dir=${dir}`,
+      `--alias=${alias}`,
+    ]);
+
+    deployProcess.stdout.pipe(process.stdout);
+    deployProcess.stderr.pipe(process.stderr);
+
+    const { stdout } = await deployProcess;
+    const previewURL = getPreviewURL(stdout);
+
+    for (const comment of comments) {
+      if (
+        comment.user.login === 'github-actions[bot]' &&
+        comment.body.startsWith('Preview is ready!') &&
+        comment.body.includes(previewURL)
+      ) {
+        this.log('Found comment %d, removing…', comment.id);
+
+        await request(
+          'DELETE /repos/:owner/:repo/issues/comments/:comment_id',
+          {
+            repo,
+            owner,
+            comment_id: comment.id,
+            headers: { authorization: `Token ${GITHUB_TOKEN}` },
+          },
+        );
+      }
+    }
+
+    await request('POST /repos/:owner/:repo/issues/:issue_number/comments', {
+      repo,
+      owner,
+      issue_number: issueNumber,
+      headers: { authorization: `Token ${GITHUB_TOKEN}` },
+      body: [
+        'Preview is ready!',
+        `Built with commit ${GITHUB_SHA}`,
+        previewURL,
+      ].join('\n'),
+    });
+  }
+}
