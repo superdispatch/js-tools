56eb0468935d6c00f8a5aef19f26ed5820e72aab
Umidbek Karimov <uma.karimov@gmail.com>
2020-11-12 17:23:31 +0500

    feat(eslint-plugin): [ts-cypress] do not extend `ts-node`.

--- packages/eslint-plugin/src/configs/ts-cypress.spec.ts
+++ packages/eslint-plugin/src/configs/ts-cypress.spec.ts
@@ -7,7 +7,7 @@ import {
 
 it('extends dependencies', async () => {
   const [meta, rules] = getConfigDataDiff(
-    await getConfigData('ts-jest'),
+    await getConfigData('typescript'),
     await getConfigData('ts-cypress'),
   );
 
@@ -18,22 +18,23 @@ it('extends dependencies', async () => {
 
     @@ --- --- @@
       Object {
-        "env": Object {
-    -     "jest/globals": true,
+    -   "env": Object {},
+    +   "env": Object {
     +     "cypress/globals": true,
     +     "jest": false,
     +     "jest/globals": false,
-          "node": true,
+    +   },
+        "globals": Object {},
+        "parser": "node_modules/@typescript-eslint/parser/dist/index.js",
+        "parserOptions": Object {
+          "ecmaVersion": 2020,
+          "sourceType": "module",
         },
-        "globals": Object {
-          "ArrayBuffer": "readonly",
-          "Atomics": "readonly",
-    @@ --- --- @@
+        "plugins": Array [
           "import",
-          "node",
           "@typescript-eslint",
-          "jest",
-          "testing-library",
+    +     "jest",
+    +     "testing-library",
     +     "cypress",
           "simple-import-sort",
           "eslint-comments",
@@ -46,6 +47,73 @@ it('extends dependencies', async () => {
     - First value
     + Second value
 
+    @@ --- --- @@
+        ],
+        "@typescript-eslint/no-empty-interface": Array [
+          "error",
+        ],
+        "@typescript-eslint/no-explicit-any": Array [
+    -     "error",
+    -     Object {
+    -       "ignoreRestArgs": true,
+    -     },
+    +     "off",
+        ],
+        "@typescript-eslint/no-extra-non-null-assertion": Array [
+          "error",
+        ],
+        "@typescript-eslint/no-extra-parens": Array [
+    @@ --- --- @@
+        ],
+        "@typescript-eslint/no-non-null-asserted-optional-chain": Array [
+          "error",
+        ],
+        "@typescript-eslint/no-non-null-assertion": Array [
+    -     "error",
+    +     "off",
+        ],
+        "@typescript-eslint/no-shadow": Array [
+          "error",
+        ],
+        "@typescript-eslint/no-this-alias": Array [
+    @@ --- --- @@
+        ],
+        "@typescript-eslint/no-unnecessary-type-assertion": Array [
+          "error",
+        ],
+        "@typescript-eslint/no-unsafe-assignment": Array [
+    -     "error",
+    +     "off",
+        ],
+        "@typescript-eslint/no-unsafe-call": Array [
+    -     "error",
+    +     "off",
+        ],
+        "@typescript-eslint/no-unsafe-member-access": Array [
+    -     "error",
+    +     "off",
+        ],
+        "@typescript-eslint/no-unsafe-return": Array [
+    -     "error",
+    +     "off",
+        ],
+        "@typescript-eslint/no-unused-expressions": Array [
+          "error",
+        ],
+        "@typescript-eslint/no-unused-vars": Array [
+    @@ --- --- @@
+          Object {
+            "checkCompoundAssignments": true,
+          },
+        ],
+        "@typescript-eslint/restrict-template-expressions": Array [
+    -     "error",
+    +     "off",
+        ],
+        "@typescript-eslint/return-await": Array [
+          "error",
+          "in-try-catch",
+        ],
     @@ --- --- @@
         "curly": Array [
           "error",
@@ -67,60 +135,157 @@ it('extends dependencies', async () => {
         "dot-notation": Array [
           "error",
     @@ --- --- @@
+          "warn",
+        ],
+        "import/no-duplicates": Array [
+          "error",
+        ],
+    +   "import/no-extraneous-dependencies": Array [
+    +     "off",
+    +   ],
+        "import/no-mutable-exports": Array [
+          "error",
+        ],
+        "import/no-named-as-default": Array [
+          "error",
+    @@ --- --- @@
+        ],
+        "indent": Array [
+          "off",
         ],
         "indent-legacy": Array [
+    +     "off",
+    +   ],
+    +   "jest/expect-expect": Array [
+    +     "off",
+    +   ],
+    +   "jest/no-alias-methods": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-commented-out-tests": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-conditional-expect": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-deprecated-functions": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-disabled-tests": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-done-callback": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-export": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-focused-tests": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-identical-title": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-interpolation-in-snapshots": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-jasmine-globals": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-jest-import": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-mocks-import": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-standalone-expect": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-test-prefixes": Array [
+    +     "error",
+    +   ],
+    +   "jest/no-try-expect": Array [
+    +     "error",
+    +   ],
+    +   "jest/prefer-to-be-null": Array [
+    +     "error",
+    +   ],
+    +   "jest/prefer-to-be-undefined": Array [
+    +     "error",
+    +   ],
+    +   "jest/prefer-to-contain": Array [
+    +     "error",
+    +   ],
+    +   "jest/prefer-to-have-length": Array [
+    +     "error",
+    +   ],
+    +   "jest/valid-describe": Array [
+    +     "error",
+    +   ],
+    +   "jest/valid-expect": Array [
+    +     "off",
+    +   ],
+    +   "jest/valid-expect-in-promise": Array [
           "off",
-        ],
-        "jest/expect-expect": Array [
-    -     "error",
-    +     "off",
-        ],
-        "jest/no-alias-methods": Array [
-          "error",
-        ],
-        "jest/no-commented-out-tests": Array [
-    @@ --- --- @@
-        ],
-        "jest/valid-describe": Array [
-          "error",
-        ],
-        "jest/valid-expect": Array [
-    -     "error",
-    +     "off",
-        ],
-        "jest/valid-expect-in-promise": Array [
-    -     "error",
-    +     "off",
-        ],
-        "jest/valid-title": Array [
-          "error",
+    +   ],
+    +   "jest/valid-title": Array [
+    +     "error",
         ],
         "jsx-quotes": Array [
-    @@ --- --- @@
-        ],
-        "template-tag-spacing": Array [
           "off",
         ],
-        "testing-library/await-async-query": Array [
-    -     "error",
-    +     "off",
-        ],
-        "testing-library/await-async-utils": Array [
-          "error",
-        ],
-        "testing-library/no-await-sync-query": Array [
+        "key-spacing": Array [
     @@ --- --- @@
         ],
-        "testing-library/prefer-presence-queries": Array [
+        "quotes": Array [
           "error",
+          "single",
+          Object {
+    -       "allowTemplateLiterals": false,
+    +       "allowTemplateLiterals": true,
+            "avoidEscape": true,
+          },
         ],
-        "testing-library/prefer-wait-for": Array [
-    -     "error",
+        "require-await": Array [
+          "off",
+    @@ --- --- @@
+        ],
+        "template-curly-spacing": Array [
+          "off",
+        ],
+        "template-tag-spacing": Array [
     +     "off",
+    +   ],
+    +   "testing-library/await-async-query": Array [
+    +     "off",
+    +   ],
+    +   "testing-library/await-async-utils": Array [
+    +     "error",
+    +   ],
+    +   "testing-library/no-await-sync-query": Array [
+    +     "error",
+    +   ],
+    +   "testing-library/no-debug": Array [
+    +     "error",
+    +   ],
+    +   "testing-library/no-dom-import": Array [
+    +     "error",
+    +     "react",
+    +   ],
+    +   "testing-library/no-wait-for-empty-callback": Array [
+    +     "error",
+    +   ],
+    +   "testing-library/prefer-find-by": Array [
+    +     "error",
+    +   ],
+    +   "testing-library/prefer-presence-queries": Array [
+    +     "error",
+    +   ],
+    +   "testing-library/prefer-wait-for": Array [
+          "off",
         ],
         "unicode-bom": Array [
           "off",
         ],
-        "use-isnan": Array [
   `);
 });
