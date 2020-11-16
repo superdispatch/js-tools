4771814ee290141528fd320fd3d2be2012ddd523
Umidbek Karimov <uma.karimov@gmail.com>
2020-11-16 18:20:33 +0500

    WIP

--- packages/js-tools/src/commands/lint/index.ts
+++ packages/js-tools/src/commands/lint/index.ts
@@ -1,14 +1,45 @@
 import { BaseLintCommand } from '../../base/BaseLintCommand';
-import LintESLint from './eslint';
-import LintPrettier from './prettier';
-import LintYarnDeduplicate from './yarn-deduplicate';
+import Listr = require('listr');
+import execa = require('execa');
 
 export default class LintAll extends BaseLintCommand {
   static description = 'Run all linters';
 
   async run() {
-    await LintYarnDeduplicate.run(this.argv, this.config);
-    await LintESLint.run(this.argv, this.config);
-    await LintPrettier.run(this.argv, this.config);
+    const { fix } = this.options;
+    const [, bin, , ...args] = process.argv;
+
+    const tasks = new Listr(
+      [
+        {
+          title: 'yarn-deduplicate',
+          task: () => execa(bin, ['lint:yarn-deduplicate', ...args]),
+        },
+
+        {
+          title: 'lint',
+          task: () =>
+            new Listr(
+              [
+                {
+                  title: 'eslint',
+                  task: () => execa(bin, ['lint:eslint', ...args]),
+                },
+                {
+                  title: 'prettier',
+                  task: () => execa(bin, ['lint:prettier', ...args]),
+                },
+              ],
+
+              { concurrent: !fix },
+            ),
+        },
+      ],
+      {
+        concurrent: true,
+      },
+    );
+
+    await tasks.run();
   }
 }
