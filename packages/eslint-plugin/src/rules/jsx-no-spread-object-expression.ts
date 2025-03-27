aa2c69df93654f1016c9b9b757f524d6bad914df
SiriusCrain <yuriy.matskanyuk@gmail.com>
2025-03-27 15:23:53 +0500

    Fixed deps

--- packages/eslint-plugin/src/rules/jsx-no-spread-object-expression.ts
+++ packages/eslint-plugin/src/rules/jsx-no-spread-object-expression.ts
@@ -1,4 +1,4 @@
-import { TSESTree } from '@typescript-eslint/typescript-estree';
+import { TSESTree } from '@typescript-eslint/types';
 import { Rule } from 'eslint';
 import { BaseNode, Node } from 'estree';
 
