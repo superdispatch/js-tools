554cfbebe1fcfbac61e245e3f566b14244431a92
Aliyev Doniyor <doniyor2109@gmail.com>
2025-05-27 10:17:18 +0500

    chore: Remove jest config from cypress

--- packages/eslint-plugin/src/configs/ts-cypress.ts
+++ packages/eslint-plugin/src/configs/ts-cypress.ts
@@ -1,32 +1,9 @@
 import { Linter } from 'eslint';
-import { injectTSJestConfig } from './ts-jest';
 import { createTypeScriptConfig } from './typescript';
-import { injectConfigs, injectEnv, injectRules } from './utils/configUtils';
+import { injectConfigs, injectRules } from './utils/configUtils';
 
 export function createTSCypressConfig(): Linter.Config {
   const config = createTypeScriptConfig();
-
-  //
-  // @superdispatch/eslint-plugin
-  //
-
-  injectTSJestConfig(config);
-  // injectRules()
-
-  //
-  // eslint-plugin-jest
-  //
-
-  injectEnv(config, {
-    jest: false,
-    'jest/globals': false,
-  });
-
-  injectRules(config, {
-    'jest/expect-expect': 'off',
-    'jest/valid-expect': 'off',
-    'jest/valid-expect-in-promise': 'off',
-  });
 
   //
   // eslint-plugin-testing-library
